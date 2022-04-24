cb1efd7c27669f56f3e24aa313f9f5235ceef71f
neksonnekson2905 <neksonnekson2905@gmail.com>
2022-04-24 02:08:59 +0200

    fourth

--- config/db.js
+++ config/db.js
@@ -6,20 +6,20 @@ const knexConfig = isProduction ?
   {
     dialect: 'pg',
     connection: {
-      host: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
-      user: process.env.SQL_USER,
-      password: process.env.SQL_PASSWORD,
-      database: process.env.SQL_DATABASE,
-    }
-  } :
-  {
-    dialect: 'pg',
-    connection: {
       host: 'localhost',
       user: 'app',
       password: 'appusertest',
       database: 'lender_api_dev'
   }
+  } :
+  {
+   dialect: 'pg',
+    connection: {
+      host: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
+      user: process.env.SQL_USER,
+      password: process.env.SQL_PASSWORD,
+      database: process.env.SQL_DATABASE,
+    }
 };
 
 module.exports = knexConfig
